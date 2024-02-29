6b6fbb1f9c2be0d294b10f49890539217d91cae2
ferdinandiz <80607754+ferdinandiz@users.noreply.github.com>
2024-02-29 19:08:00 -0300

    Feature: - Botões Radio

--- app/src/main/java/com/fer/aula02/MainActivity.java
+++ app/src/main/java/com/fer/aula02/MainActivity.java
@@ -3,6 +3,7 @@ package com.fer.aula02;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
@@ -12,10 +13,12 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText ed01, ed02, ed03;
+    EditText ed01, ed02;
     TextView textoAlvo;
     ImageView btn;
-    String op;
+    String op = "+";
+    RadioButton mais, menos, vezes, div;
+    RadioGroup operacoes;
     int resp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,19 +27,36 @@ public class MainActivity extends AppCompatActivity {
         ed01 = findViewById(R.id.ed01);
         ed02 = findViewById(R.id.ed02);
         textoAlvo = findViewById(R.id.alvo);
-        ed03 = findViewById(R.id.ed03);
         btn = findViewById(R.id.btn01);
+        mais = findViewById(R.id.mais);
+        menos = findViewById(R.id.menos);
+        vezes = findViewById(R.id.vezes);
+        div = findViewById(R.id.divide);
+        operacoes = findViewById(R.id.ed03);
 
+
+        operacoes.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == mais.getId())
+                    op = "+";
+                else if (checkedId == menos.getId()) {
+                    op = "-";
+                } else if (checkedId == vezes.getId()) {
+                    op = "*";
+                }else {
+                    op = "/";
+                }
+            }
+        });
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(!ed01.getText().toString().isEmpty()
-                && !ed02.getText().toString().isEmpty()
-                && !ed03.getText().toString().isEmpty()){
+                && !ed02.getText().toString().isEmpty()){
 
                     resp = Integer.parseInt(ed01.getText().toString());
-                    op = ed03.getText().toString();
                     if (op.equals("+") ) resp += Integer.parseInt(ed02.getText().toString());
                     else if (op.equals("-")) resp -= Integer.parseInt(ed02.getText().toString());
                     else if (op.equals("*")) resp *= Integer.parseInt(ed02.getText().toString());
